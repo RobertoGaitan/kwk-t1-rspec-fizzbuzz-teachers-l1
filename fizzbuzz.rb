b4a0c2ac94ca99956d567ed8b91c72bb56beef18
Roberto Gaitan <rgaitan.it@gmail.com>
2018-05-26 20:31:37 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+require_relative './spec_helper.rb'
+require './fizzbuzz.rb'
+
+def fizzbuzz(number)
+  if number % 3 == 0
+    "Fizz"
+  else if number % 5 == 0
+  "Buzz"
+else "FizzBuzz"
+end
+end
+end
